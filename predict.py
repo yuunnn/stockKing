227007a96ea2f2a8115752ef27b89c9df241d8f0
yuunnn <wooxy610@icloud.com>
2022-11-03 01:08:21 +0800

    add indices and industry embedding

--- predict.py
+++ predict.py
@@ -36,7 +36,7 @@ def predict(model_file, predicts_file):
 if __name__ == "__main__":
     probs, stock_codes = predict('./models/model_1667405664.pkl', './predictset/latest.csv')
     df = pd.DataFrame(probs.detach().numpy())
-    df.columns = ['不持有', '买入', '持有', '卖出']
+    df.columns = ['观望', '买入', '持有', '卖出']
     df['code'] = stock_codes
     dt = datetime.date.today().strftime('%Y%m%d')
     df.to_csv('./predictset/output{}.csv'.format(dt), index=False)
